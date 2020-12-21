6ba9f180b36f45bb67fe2c21ed1a99dac57f7248
bappychanting <bappychanting@outlook.com>
2020-12-21 10:13:21 +0600

    updated sitemap class

--- Classes/Sitemap.php
+++ Classes/Sitemap.php
@@ -20,8 +20,53 @@ class Sitemap
       $urlset->appendChild($url);
     }
 
+    // Add URL node
+    public function addNode($location='', $last_modified='') 
+    {
+      try{
+        if(!empty($location) && !empty($last_modified)){
+          $this->dom->load('sitemap.xml');
+          $this->appendUrl($location, $last_modified);
+          $this->dom->save('sitemap.xml');
+        }
+        return TRUE;
+      }
+      catch (\Exception $e) {
+        return $e->getMessage();
+      }
+    }
+
+    // Update current URL nodes
+    public function updateNodes($exclutions=array(), $urls=array()) 
+    {
+      try{
+        $this->dom->load('sitemap.xml');
+        $this->appendUrl(APP_URL, date('Y-m-d'));
+
+        $routes = include("routes/web.php");
+        foreach ($routes as $key => $value) {
+          if(count(array_intersect(array_map('strtolower', explode('/', $key)), $exclutions)) == 0){
+            $this->appendUrl(route($key), date('Y-m-d'));
+          }
+        }
+
+        if(count($urls) > 0){
+          foreach ($urls as $url) {
+            $this->appendUrl($url[0], $url[1]);
+          }
+        }
+
+        $this->dom->save('sitemap.xml');
+        return $urls;
+      }
+      catch (\Exception $e) {
+        return $e->getMessage();
+      }
+    }
+
     // Delete current URL nodes
-    public function deleteNodes() {
+    public function deleteNodes() 
+    {
       try{
         $this->dom->load('sitemap.xml');
         $matchingElements = $this->dom->getElementsByTagName('url');
@@ -41,37 +86,11 @@ class Sitemap
       }
     }
 
-    // Update current URL nodes
-    public function updateNodes($exclutions=array(), $urls=array()) {
-      try{
-        $this->dom->load('sitemap.xml');
-        $this->appendUrl(APP_URL, date('Y-m-d'));
-
-        $routes = include("routes/web.php");
-        foreach ($routes as $key => $value) {
-          if(count(array_intersect(array_map('strtolower', explode('/', $key)), $exclutions)) == 0){
-            $this->appendUrl(APP_URL.'/'.$key, date('Y-m-d'));
-          }
-        }
-
-        if(count($urls) > 0){
-          foreach ($urls as $url) {
-            $this->appendUrl($url[0], $url[1]);
-          }
-        }
-
-        $this->dom->save('sitemap.xml');
-        return $urls;
-      }
-      catch (\Exception $e) {
-        return $e->getMessage();
-      }
-    }
-
-    public function rewriteXml($exclutions=array(), $urls=array()) {
+    public function refreshSitemap($exclutions=array(), $urls=array()) 
+    {
       $this->deleteNodes();
-      $this->updateNodes($exclutions, $urls);
-      return TRUE;
+      $urls = $this->updateNodes($exclutions, $urls);
+      return $urls;
     }
 
 }
